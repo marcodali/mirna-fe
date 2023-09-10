e812495a2ac670ba6ad0d642343760a748cfdc74
Marco Dali <marco@durazno.io>
2023-09-10 06:15:09 -0600

    better documented example src

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 
-export const REAL_API = 'https://api.mirna.cloud/socket'
+export const REAL_API = 'https://api.mirna.cloud/sockets'
+//export const REAL_API = 'http://localhost:4000/sockets'
 export const RANDOM_USER_API = 'https://randomuser.me/api/?inc=login&noinfo'
 export const DURAZNO_WEBSITE = 'https://durazno.io'
 export const DURAZNO_NAME = 'Durazno Technologies'
