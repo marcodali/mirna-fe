fc4c1cd4cb08960e6fa54843e0785c4fa2afdebe
Marco Dali <marco@durazno.io>
2023-10-27 05:11:19 -0600

    more clear instructions

--- components/Instructions.js
+++ components/Instructions.js
@@ -3,15 +3,15 @@ export default function Instructions() {
         <div className="note">
             <h5>Some Tips:</h5>
             <ul>
-                <li>This is not a socket.io  server only ws library work</li>
-                <li>Not listening to ports, see example project</li>
-                <li>Do not use require(), Use ES6 syntax always import from</li>
-                <li>The only one installed npm package that you can import now is ws version 8.13.0</li>
-                <li>No typescript, just vanilla JavaScript</li>
-                <li>You don't need to enable CORS, it's explicitly enabled by default for * origins</li>
-                <li>Don't use var, only let and const</li>
-                <li>If your code does not compile you will not get a wss url</li>
-                <li>If your code has an unhandled exception it will be deleted and the wss url previously generated will be deactivated</li>
+                <li>Socket.io doesn't work here =&gt; We use the <a href="https://www.npmjs.com/package/ws" target="_blank">ws</a> library</li>
+                <li>Do not open or listen to any ports =&gt; Follow the example projects</li>
+                <li>Do not use require() =&gt; Use ES6 syntax (import from)</li>
+                <li>List of npm packages that you can use/import =&gt; axios, moment, lodash, jsonwebtoken and uuid</li>
+                <li>No typescript =&gt; We prefer vanilla JavaScript</li>
+                <li>You don't worry about CORS =&gt; It's explicitly enabled by default for * origins</li>
+                <li>Don't use var =&gt; We code only with let and const</li>
+                <li>If your code does not compile =&gt; We'll let you know but the WSS url won't work</li>
+                <li>If your code rises an exception you MUST catch it, if not =&gt; your code will be deleted and the WSS url previously generated will be deactivated</li>
             </ul>
         </div>
     )
