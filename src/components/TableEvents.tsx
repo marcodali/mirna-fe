b9de7d018892ab00f19a4ef167c8b2108bc7e027
Marco Dali <marco@durazno.io>
2023-11-11 21:18:32 -0600

    funciona chida la creacion de sockets

--- src/components/TableEvents.tsx
+++ src/components/TableEvents.tsx
@@ -7,27 +7,44 @@ type TableEventsProps = {
 }
 
 export default function TableEvents({ url }: TableEventsProps) {
-    const webSocketRef = useRef<WebSocket | null>(null)
     const [tablaData, setTablaData] = useState<string[]>([])
 
+    // Se asigna el nuevo WebSocket a la referencia.
+    const webSocketRef = useRef<WebSocket | null>(null)
+
     useEffect(() => {
+        // Se asigna el nuevo WebSocket a la referencia.
+        webSocketRef.current = new WebSocket(url)
 
-        // Inicializar WebSocket solo si aún no está abierto o no existe
-        if (!webSocketRef.current || webSocketRef.current.readyState === WebSocket.CLOSED) {
-            webSocketRef.current = new WebSocket(url)
-            webSocketRef.current.onopen = () => console.log('Conexión abierta')
-            webSocketRef.current.onerror = (error: Event) => console.error(error)
-            webSocketRef.current.onclose = () => console.log('Conexión cerrada')
+        // Se asegura de que el WebSocket se ha creado antes de añadir event listeners.
+        if (webSocketRef.current) {
+            webSocketRef.current.onopen = () => {
+                console.log('WebSocket connection established')
+            }
 
-            webSocketRef.current.onmessage = (event: MessageEvent) => setTablaData(
-                (oldData) => [...oldData, event.data]
-            )
+            webSocketRef.current.onmessage = (event: MessageEvent) => {
+                console.log('Message from server', event.data)
+                setTablaData(
+                    (oldData) => [...oldData, event.data]
+                )
+            }
+
+            webSocketRef.current.onerror = (event: Event) => {
+                console.error('WebSocket error', event)
+            }
+
+            webSocketRef.current.onclose = (event: CloseEvent) => {
+                console.log('WebSocket connection closed', event.reason)
+            }
         }
 
-        // Limpieza al desmontar el componente
-        return () => webSocketRef.current?.close()
-
-    }, [url]) // Execute every time url changes
+        // Función de limpieza para cerrar el WebSocket
+        return () => {
+            if (webSocketRef.current) {
+                webSocketRef.current.close()
+            }
+        }
+    }, []) // Se ejecuta solo una vez
 
     return (
         <table>
