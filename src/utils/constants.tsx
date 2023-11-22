9e0fcfe99d9bf93d872332996b128b672f332df5
Marco Dali <marco@durazno.io>
2023-11-22 05:51:47 -0600

    better example project

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -10,10 +10,11 @@ export const GET_CODE_API = 'http://localhost:4000/sockets/1?username=__USERNAME
 export const WSS_PROJECT_URI = 'ws://localhost:4000/__USERNAME__/__PROJECT__' */
 
 // general
+const sampleFiles = ['/broadcastServerSample.txt', '/echoServerSample.txt', '/librariesUsage.txt']
+export const CODE_SAMPLE = sampleFiles[Math.floor(Math.random() * sampleFiles.length)]
 export const RANDOM_USER_API = 'https://randomuser.me/api/?inc=login&noinfo'
 export const DURAZNO_WEBSITE = 'https://durazno.io'
 export const DURAZNO_NAME = 'Durazno Technologies'
-export const CODE_SAMPLE = Math.random() > 0.5 ? '/broadcastServerSample.txt' : '/echoServerSample.txt'
-export const SERVER_PROJECT_EXAMPLE = 'https://gist.github.com/marcodali/3a11ed909c864e98cc1639106acdc93f'
+export const SERVER_PROJECT_EXAMPLE = 'https://gist.github.com/marcodali/f71c88953f648d9c90fe25fd254f88c4'
 export const CLIENT_PROJECT_EXAMPLE = 'https://playcode.io/1604700'
 export const LIVE_DEMO_PROJECT_EXAMPLE = 'https://1604700.playcode.io'
